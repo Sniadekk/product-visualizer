6ad882c60841741abef702932d016791a7153696
Sniadek <wojokarolg9@gmail.com>
2018-04-10 16:46:25 +0200

    Added light helper and shadows.

---
+++ js/walls.js
@@ -0,0 +1,34 @@
+class Walls {
+  constructor() {
+    this.material = new THREE.MeshStandardMaterial({
+      visible: true,
+      color: "green",
+      emissive: 1.5,
+      side: THREE.DoubleSide
+    });
+    this.meshes = [];
+    this.initWalls();
+  }
+
+  initWalls() {
+    const walls = [
+      { size: { x: 6, y: 0.1, z: 6 }, position: { x: 0, y: 0, z: 0 } },
+      { size: { x: 6, y: 6, z: 0.1 }, position: { x: 0, y: 3, z: -3 } },
+     // { size: { x: 6, y: 6, z: 0.1 }, position: { x: 0, y: 3, z: 3 } },
+     { size: { x: 0.1, y: 6, z: 6 }, position: { x: 3, y: 3, z: 0 } },
+      { size: { x: 0.1, y: 6, z: 6 }, position: { x: -3, y: 3, z: 0 } },
+      { size: { x: 6, y: 0.1, z: 6 }, position: { x: 0, y: 6, z: 0 } },
+    ];
+    walls.map(wall => {
+      const geometry = new THREE.BoxGeometry(
+        wall.size.x,
+        wall.size.y,
+        wall.size.z
+      );
+      const cube = new THREE.Mesh(geometry, this.material);
+      cube.receiveShadow = true;
+      cube.position.set(wall.position.x, wall.position.y, wall.position.z);
+      this.meshes.push(cube);
+    });
+  }
+}
